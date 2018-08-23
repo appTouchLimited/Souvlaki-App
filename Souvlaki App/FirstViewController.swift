120efc51898ca45d3a26f1c5a282bf11e06f80ba
user912996 <user912996@87-237-62-8.rdns.melbourne.co.uk>
2018-08-23 01:36:42 -0700

    Initial Commit

---
+++ Souvlaki App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Souvlaki App
+//
+//  Created by user912996 on 8/23/18.
+//  Copyright © 2018 user912996. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Souvlaki AppTests/Souvlaki_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Souvlaki_AppTests.swift
+//  Souvlaki AppTests
+//
+//  Created by user912996 on 8/23/18.
+//  Copyright © 2018 user912996. All rights reserved.
+//
+
+import XCTest
+@testable import Souvlaki_App
+
+class Souvlaki_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
